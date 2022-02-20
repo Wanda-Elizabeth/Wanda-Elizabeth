2ca8050b2c571749d3b3a42ceb6f8b06cb957dd8
WandaElizabeth <wandawairimu@gmail.com>
2022-02-20 22:30:47 +0300

    add my file

---
+++ src/main.kt
@@ -0,0 +1,23 @@
+fun main(){
+    helloname()
+    rem(20,3)
+    num(2,3,4,7,)
+    fact()
+}
+fun helloname(){
+    val name = "Wanda"
+    println("Hello "+name)
+}
+fun rem(a:Int,b:Int){
+    var remainder =a/b
+    println(remainder)
+
+}
+
+ fun num(a:Int,b:Int,c:Int,d:Int){
+     var sum = a+b+c+d
+     println(sum)
+ }
+fun fact(){
+    println("i love singing")
+}
